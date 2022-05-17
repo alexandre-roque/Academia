133ad2a4e9b362087710176bcb9e42c5fc079f17
Alexandre Roque <alexandre.roque@sydle.com>
2022-05-17 08:53:47 -0300

    fix: title

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -4,8 +4,8 @@ import { planos } from '../../test'
 
 export const Cards = () => {
   return (
-      <>
-      {planos.map((plano) => {
+    <>
+        {planos.map((plano) => {
             return (
                 <div className='w-full py-[4rem] px-4 bg-white'>
                     <div className='py-5 text-center text-4xl font-black uppercase'>{plano.name}</div>
@@ -20,7 +20,7 @@ export const Cards = () => {
                     </div>
                 </div>
             );
-      })}
-      </>
+        })}
+    </>
   );
 };
