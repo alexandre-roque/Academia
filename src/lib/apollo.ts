c65f03b306146393d1d5936bdaf99d4e91797bd5
Ronaldo Mendonça Zica <ronaldomendoncazica@gmail.com>
2022-07-07 20:18:19 -0300

    final

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: "https://api-sa-east-1.graphcms.com/v2/cl4q5ue0v30hy01xsfnghazto/master",
+    uri: "https://api-sa-east-1.graphcms.com/v2/cl5bm3g0q0ark01um88eng9oy/master",
     cache: new InMemoryCache(),
 });
